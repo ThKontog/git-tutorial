e776a1d422e25e4609321607b24d2d33e9f6bb97
ThKontog <th.kontogeorg@gmail.com>
2022-10-14 16:17:10 +0300

    Version 2

--- Git-tutorial-File proj/Program.cs
+++ Git-tutorial-File proj/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("version1");
+Console.WriteLine("Hello!");
+Console.WriteLine("version2");
