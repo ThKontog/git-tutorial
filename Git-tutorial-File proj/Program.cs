128302a5f26fba2280e550cb6c7ac73af457d03f
ThKontog <th.kontogeorg@gmail.com>
2022-10-17 16:44:33 +0300

    Restore all files

---
+++ Git-tutorial-File proj/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello bye");
+Console.WriteLine("version3");
+
+Hello helloClass = new Hello();
+
+helloClass.HelloMeth();
